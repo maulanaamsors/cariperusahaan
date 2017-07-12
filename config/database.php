43690b29f2afa860034d52facff1ccb1c6497a21
faisalishak <faisal.ishak92@outlook.com>
2017-07-12 17:11:52 +0700

    [Update] database.config

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => 'us-cdbr-azure-southcentral-f.cloudapp.net',
+            'port' => '3306',
+            'database' => 'concerns',
+            'username' => 'b59f65860779a5',
+            'password' => '8881d6ff',
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
