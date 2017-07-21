b611e78f68255c71ab21a7f2e76c3921eb8c6948
faisalishak <faisal.ishak92@outlook.com>
2017-07-21 11:26:29 +0700

    [UPDATE] olah pemilik controller

--- app/Http/Controllers/pengolahanakunpengusahahController.php
+++ app/Http/Controllers/pengolahanakunpengusahahController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Pemilik_usaha;
-
+use Mail;
+use App\Mail\ActivateUser;
 
 class pengolahanakunpengusahahController extends Controller
 {
@@ -19,6 +20,11 @@ class pengolahanakunpengusahahController extends Controller
         if ($req->active == '0'){ 
             Pemilik_usaha::where('id_pemilik', $req->id_pemilik)
             ->update(['active' => '1' ]);
+
+            $pemilik=Pemilik_usaha::where('id_pemilik', $req->id_pemilik)-first();
+            
+
+            Mail::to($pemilik->email)->send(new ActivateUser());
         }else{
             Pemilik_usaha::where('id_pemilik', $req->id_pemilik)
             ->update(['active' => '0' ]);
--- resources/views/emails/user/activate.blade.php
+++ resources/views/emails/user/activate.blade.php
@@ -1,10 +1,12 @@
 @component('mail::message')
 # Your Account has been active
 
-please log in
+Admin from concerns app has approve your request, 
+<br>
+you can create your company profile right now. 
 
 @component('mail::button', ['url' => 'https://concerns.azurewebsites.net/'])
-Log in
+Log in here
 @endcomponent
 
 Thanks,<br>
